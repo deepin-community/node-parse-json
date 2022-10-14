39cc191071d160c03b3595be1a5b8ac449c1bf33
Deepin Developer <deepin-dev@deepin.org>
2022-10-14 13:49:13 +0800

    feat: Init commit
    init node-parse-json
    
    Log:

---
+++ lines-and-columns/src/index.ts
@@ -0,0 +1,77 @@
+export type SourceLocation = {
+  line: number,
+  column: number
+};
+
+const LF = '\n';
+const CR = '\r';
+
+export default class LinesAndColumns {
+  private string: string;
+  private offsets: Array<number>;
+
+  constructor(string: string) {
+    this.string = string;
+
+    let offsets = [0];
+
+    for (let offset = 0; offset < string.length;) {
+      switch (string[offset]) {
+        case LF:
+          offset += LF.length;
+          offsets.push(offset);
+          break;
+
+        case CR:
+          offset += CR.length;
+          if (string[offset] === LF) {
+            offset += LF.length;
+          }
+          offsets.push(offset);
+          break;
+
+        default:
+          offset++;
+          break;
+      }
+    }
+
+    this.offsets = offsets;
+  }
+
+  locationForIndex(index: number): SourceLocation | null {
+    if (index < 0 || index > this.string.length) {
+      return null;
+    }
+
+    let line = 0;
+    let offsets = this.offsets;
+
+    while (offsets[line + 1] <= index) {
+      line++;
+    }
+
+    let column = index - offsets[line];
+    return { line, column };
+  }
+
+  indexForLocation(location: SourceLocation): number | null {
+    let { line, column } = location;
+
+    if (line < 0 || line >= this.offsets.length) {
+      return null;
+    }
+
+    if (column < 0 || column > this.lengthOfLine(line)) {
+      return null;
+    }
+
+    return this.offsets[line] + column;
+  }
+
+  private lengthOfLine(line: number): number {
+    let offset = this.offsets[line];
+    let nextOffset = line === this.offsets.length - 1 ? this.string.length : this.offsets[line + 1];
+    return nextOffset - offset;
+  }
+}
---
+++ test.js
@@ -0,0 +1,37 @@
+import test from 'ava';
+import parseJson from '.';
+
+const jsonErrorRegex = /Unexpected token "}".*in foo\.json/;
+
+test('main', t => {
+	t.truthy(parseJson('{"foo": true}'));
+
+	t.throws(() => {
+		parseJson('{\n\t"foo": true,\n}');
+	}, {
+		name: 'JSONError',
+		message: /Unexpected token "}"/
+	});
+
+	t.throws(() => {
+		try {
+			parseJson('{\n\t"foo": true,\n}');
+		} catch (error) {
+			error.fileName = 'foo.json';
+			throw error;
+		}
+	}, jsonErrorRegex);
+
+	t.throws(() => {
+		parseJson('{\n\t"foo": true,\n}', 'foo.json');
+	}, jsonErrorRegex);
+
+	t.throws(() => {
+		try {
+			parseJson('{\n\t"foo": true,\n}', 'bar.json');
+		} catch (error) {
+			error.fileName = 'foo.json';
+			throw error;
+		}
+	}, jsonErrorRegex);
+});
